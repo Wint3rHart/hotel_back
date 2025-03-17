85e0c8ae6a3a6a2e416a86e13d25c311f0cb6148
Wint3rHart <misterhassan53@gmail.com>
2025-03-17 22:27:16 +0500

    Add files via upload

--- Cloud.js
+++ Cloud.js
@@ -1,9 +1,1 @@
-let cloud=require('cloudinary').v2;
-
-cloud.config({cloud_name:"dfibwqcmx",api_key:'537772646948454',api_secret:'V2kci-CL6SHsVogWgZX0GWwI4MI'})
-
-
-const cloud_fnx=async(path,public,folder)=>{  let upload=await cloud.uploader.upload(path,{folder:`/Hotel-pics/${folder}`,public_id:public});let url=cloud.url(upload.public_id,{height:400,width:500,crop:'fill',gravity:'auto'}); return [upload,url]  }
-
-
-module.exports=cloud_fnx
+"not present sice not needed "
